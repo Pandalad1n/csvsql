c7bb0828af8707704b055cce1735ae35829d1859
Pandalad1n <Xpandalad1nX@gmail.com>
2019-08-09 22:55:31 +0300

    add insert test

--- app/processor.py
+++ app/processor.py
@@ -6,14 +6,36 @@ class Processor:
         self.columns = columns
         self.connection = connection
 
-    def process(self):
+    def create(self):
+        sql = """
+            CREATE TABLE {table_name} ({columns_sql});
+        """.format(
+            table_name=self.name,
+            columns_sql=self.columns_types_sql(),
+        )
         with self.connection.cursor() as c:
-            c.execute("""
-                CREATE TABLE {table_name} ({columns_sql});
-            """.format(
-                table_name=self.name,
-                columns_sql=self.columns_sql(),
-            ))
+            c.execute(sql)
+
+    def insert(self):
+        sql = """
+            INSERT INTO {table_name} ({columns})
+            VALUES {rows};
+        """.format(
+            table_name=self.name,
+            columns=self.columns_sql(),
+            rows=self.rows_sql(),
+        )
+        with self.connection.cursor() as c:
+            c.execute(sql)
+
+    def columns_types_sql(self):
+        return ",".join([c + " VARCHAR" for c in self.columns])
 
     def columns_sql(self):
-        return ",".join([c + " VARCHAR" for c in self.columns])
+        return ", ".join([c for c in self.columns])
+
+    def rows_sql(self):
+        return ", ".join(self.row_sql(r) for r in self.rows)
+
+    def row_sql(self, row):
+        return "(" + ", ".join("'" + c + "'" for c in row) + ")"
--- app/test.py
+++ app/test.py
@@ -31,11 +31,23 @@ class TestDB(unittest.TestCase):
         columns = ("banme", "mane", "lmene")
         name = "bibia"
         proc = Processor(self.conn, name, columns, [])
-        proc.process()
+        proc.create()
         with self.conn.cursor() as c:
             c.execute("SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{}'".format(name))
             resp_columns = c.fetchall()
             self.assertEqual([(c,) for c in columns], resp_columns)
+
+    def test_fills_data(self):
+        columns = ("banme", "mane", "lmene")
+        name = "bibia"
+        rows = ("bobo1", "baba1", "bebe1"), ("bobo2", "baba2", "bebe2"), ("bobo3", "baba3", "bebe3")
+        proc = Processor(self.conn, name, columns, rows)
+        proc.create()
+        proc.insert()
+        with self.conn.cursor() as c:
+            c.execute("SELECT * FROM {}".format(name))
+            resp = c.fetchall()
+            self.assertEqual([r for r in rows], resp)
 
 
 if __name__ == '__main__':
