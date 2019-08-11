c0d865bdcf4a0462f0e35f2115f89f6d7ace0659
Pandalad1n <Xpandalad1nX@gmail.com>
2019-08-11 21:45:56 +0300

    add xlsx handler

--- app/provider.py
+++ app/provider.py
@@ -1,4 +1,5 @@
 import csv
+import openpyxl
 
 
 class CSVProvider:
@@ -20,4 +21,27 @@ class CSVProvider:
             return self.rows_cache
         reader = csv.reader(self.csv_file)
         self.rows_cache = tuple(tuple(row) for row in reader)
+        return self.rows_cache
+
+
+class XlsxProvider:
+    def __init__(self, xlsx_file):
+        self.columns_cache = None
+        self.rows_cache = None
+        self.book = openpyxl.load_workbook(xlsx_file)
+
+    def columns(self):
+        if self.columns_cache:
+            return self.columns_cache
+        sheet = self.book.active
+        self.columns_cache = tuple(v.value for v in next(sheet.rows))
+        return self.columns_cache
+
+    def rows(self):
+        if self.rows_cache:
+            return self.rows_cache
+        sheet = self.book.active
+        rows = sheet.rows
+        next(rows)
+        self.rows_cache = tuple(tuple(v.value for v in r) for r in rows)
         return self.rows_cache
--- app/test/test_provider.py
+++ app/test/test_provider.py
@@ -1,13 +1,9 @@
-from provider import CSVProvider
+from provider import CSVProvider, XlsxProvider
 import unittest
 import io
+from openpyxl import Workbook
 
-
-
-class TestProvider(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestProvider, self).__init__(*args, **kwargs)
-
+class TestCSVProvider(unittest.TestCase):
     def test_data(self):
         csv_file = io.StringIO(
 """biba,baba,buba
@@ -17,3 +13,21 @@ sss,333,4444
         prov = CSVProvider(csv_file)
         self.assertEqual(("biba", "baba", "buba"), prov.columns())
         self.assertEqual((("ttt", "111", "2222"), ("sss", "333", "4444")), prov.rows())
+
+
+class TestXlsxProvider(unittest.TestCase):
+    def test_data(self):
+
+        book = Workbook()
+        sheet = book.active
+        file = io.BytesIO()
+
+        sheet.append(['biba', 'baba', 'buba'])
+        sheet.append(['This is A2', 'This is B2', 'This is C2'])
+        sheet.append(['This is A3', 'This is B3', 'This is C3'])
+
+        book.save(file)
+
+        prov = XlsxProvider(file)
+        self.assertEqual(("biba", "baba", "buba"), prov.columns())
+        self.assertEqual((('This is A2', 'This is B2', 'This is C2'), ('This is A3', 'This is B3', 'This is C3')), prov.rows())
